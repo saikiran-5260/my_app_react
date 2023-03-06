693133e2eb25b071c72c02953b17293367a662fc
saikiran-5260 <shanmukha.pithani@accionlabs.com>
2023-03-06 12:12:29 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <App s1="Sai Kiran" s2="Shanmukha" s3="Pithani" num={3} />
   </React.StrictMode>
 );
 
